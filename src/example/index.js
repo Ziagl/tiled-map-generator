994ad24b9f1ef0df91bfbcff6d57eb3403be653c
Ziagl <office@ziegelwanger-edv.at>
2024-06-01 20:02:05 +0200

    added example how to use project

---
+++ src/example/index.js
@@ -0,0 +1,27 @@
+// this example shows how to generate a map
+// to run it you need current version of Node.js
+// command: node index.js
+// output is a new map.json file that can be opened with Tiled (https://www.mapeditor.org/)
+// the folder should contain one Tiled map file with name example.json (example given)
+const fs = require('fs');
+const tmg = require('tiled-map-generator');
+
+// generate a new map
+let generator = new tmg.Generator();
+generator.generateMap(tmg.MapType.CONTINENTS_ISLANDS, tmg.MapSize.TINY);
+
+// convert this map into a 2d array
+let converter = new tmg.Converter();
+
+// store it in a new json file based on example.json
+const data = fs.readFileSync('example.json', 'utf8');
+const [map, rows, columns] = generator.exportMap();
+const result = converter.convertToTiled(map, rows, columns, data);
+fs.writeFileSync('map.json', result, 'utf-8', (err) => {
+    if (err) {
+        console.error('Error while writing map file:', err);
+    }
+});
+
+// print out map as debug output on console
+console.log(generator.print());
